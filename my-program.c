37004cf373f052cc1e755a9d029a1d0bc05b5db8
m24601 <63182964+m24601@users.noreply.github.com>
2020-10-05 18:46:24 +0800

    Update my-program.c

--- my-program.c
+++ my-program.c
@@ -1,140 +1,99 @@
 #include<stdio.h>
 #include<string.h>
-char buffer[10000];
-int intbuffer;
-FILE *file;
-int check(char buf[],int num,char keyword[])
+char input[100000],buffer[100000];
+char input_letter[]={'+','*',',','(',')'};
+char *output_letter[]={"Plus\0","Star\0","Comma\0","Lparenthesis\0","RParenthesis\0"};
+char *sp_input[]={"BEGIN\0","END\0","FOR\0","IF\0","THEN\0","ELSE\0"};
+char *sp_output[]={"Begin\0","End\0","For\0","If\0","Then\0","Else\0"};
+int token_equals(char *token1,char *token2)
 {
-	if(strlen(keyword)!=num+1)
+	if(strlen(token1)!=strlen(token2))
 		return 0;
-	for(int i=0;i<=num;i++)
+	for (int i=0;i<strlen(token1);i++)
 	{
-		if(buf[i]!=keyword[i])
+		if(token1[i]!=token2[i])
 			return 0;
 	}
 	return 1;
 }
 int isLetter(char letter)
 {
-	if('a'<=letter&&letter<='z'||'A'<=letter&&letter<='Z'||'0'<=letter&&letter<='9')
-		return 1;
-	return 0;
+	return ('a'<=letter&&letter<='z')||('A'<=letter&&letter<='Z')||('0'<=letter&&letter<='9');
 }
-int main(int argc,char * argv[])
+int main(int argc,char *argv[])
 {
-	file=fopen(argv[1],"r");
-	char input[10000];
-	while(fgets(input,10000,file)!=NULL)
+	FILE *file=fopen(argv[1],"r");
+	int num=fread(input,sizeof(char),100000,file);
+	for(int i=num;i<100000;i++)
+		input[i]='\0';
+	for(int i=0;i<strlen(input);i++)
 	{
-			for(int i=0;i<strlen(input);i++)
-	{
-		if(input[i]==' '||input[i]=='\n'||(input[i]=='\r'&&input[i]=='\n')){}
-		else if(input[i]=='+')
-			fprintf(stdout,"Plus\n");
-		else if(input[i]=='*')
-			fprintf(stdout,"Star\n");
-		else if(input[i]==',')
-			fprintf(stdout,"Comma\n");
-		else if(input[i]=='(')
-			fprintf(stdout,"LParenthesis\n");
-		else if(input[i]==')')
-			fprintf(stdout,"RParenthesis\n");
+		int check=0;
+		for(int j=0;j<5;j++)
+		{
+			if(input[i]==input_letter[j])
+			{
+				printf("%s\n",output_letter[j]);
+				check=1;
+				break;
+			}
+		}
+		if(check)
+			continue;
+		if(input[i]==' '||input[i]=='\n'||(input[i]=='\r'&&input[i]=='\n')){continue;}
 		else if(input[i]==':')
 		{
 			if(i+1<strlen(input)&&input[i+1]=='=')
 			{
+				printf("Assign\n");
 				i++;
-				fprintf(stdout,"Assign\n");
 			}
 			else
-			{
-				fprintf(stdout,"Colon\n");
-			}
+				printf("Colon\n");
+			continue;
 		}
 		else if('0'<=input[i]&&input[i]<='9')
 		{
-			int j;
-			intbuffer=input[i]-'0';
-			for(j=i+1;j<strlen(input);j++)
+			int num=input[i]-'0';
+			while('0'<=input[i+1]&&input[i+1]<='9')
 			{
-				if('0'<=input[j]&&input[j]<='9')
-				{
-					intbuffer*=10;
-					intbuffer+=input[j]-'0';
-				}
-				else
-					break;
+				i++;
+				num*=10;
+				num+=input[i]-'0';
 			}
-			i=j-1;
-			fprintf(stdout,"Int(%d)\n",intbuffer);
+			printf("Int(%d)\n",num);
+			continue;
 		}
-		else if('a'<=input[i]&&input[i]<='z'||'A'<=input[i]&&input[i]<='Z')
+		else if(('a'<=input[i]&&input[i]<='z')||('A'<=input[i]&&input[i]<='Z'))
 		{
-			int top=0,j;
-			buffer[top]=input[i];
-			for(j=i+1;j<strlen(input);j++)
+			int top=-1;
+			buffer[++top]=input[i];
+			while(isLetter(input[i+1]))
 			{
-				if(isLetter(input[j]))
+				buffer[++top]=input[++i];
+				for(int j=0;j<6;j++)
 				{
-					buffer[++top]=input[j];
-					if(check(buffer,top,"BEGIN\0")&&(j+1>=strlen(input)||!isLetter(input[j+1])))
+					if(token_equals(buffer,sp_input[j])&&(i+1>=strlen(input)||!isLetter(input[i+1])))
 					{
-						fprintf(stdout,"Begin\n");
+						printf("%s\n",sp_output[j]);
 						top=-1;
 						break;
 					}
-					if(check(buffer,top,"END\0")&&(j+1>=strlen(input)||!isLetter(input[j+1])))
-					{
-						fprintf(stdout,"End\n");
-						top=-1;
-						break;
-					}
-					if(check(buffer,top,"FOR\0")&&(j+1>=strlen(input)||!isLetter(input[j+1])))
-					{
-						fprintf(stdout,"For\n");
-						top=-1;
-						break;
-					}
-					if(check(buffer,top,"IF\0")&&(j+1>=strlen(input)||!isLetter(input[j+1])))
-					{
-						fprintf(stdout,"If\n");
-						top=-1;
-						break;
-					}
-					if(check(buffer,top,"THEN\0")&&(j+1>=strlen(input)||!isLetter(input[j+1])))
-					{
-						fprintf(stdout,"Then\n");
-						top=-1;
-						break;
-					}
-					if(check(buffer,top,"ELSE\0")&&(j+1>=strlen(input)||!isLetter(input[j+1])))
-					{
-						fprintf(stdout,"Else\n");
-						top=-1;
-						break;
-					}
-				}
-				else
-				{
-					buffer[top+1]='\0';
-					break;
 				}
 			}
 			if(top!=-1)
 			{
-				fprintf(stdout,"Ident(%s)\n",buffer);
-				i=j-1;
+				printf("Ident(%s)\n",buffer);
 			}
-			else
-				i=j;
+			for(int i=0;i<100000;i++)
+			{
+				buffer[i]=0;
+			}
 		}
 		else
 		{
-			fprintf(stdout,"Unknown\n");
+			printf("Unknown\n");
 			return 0;
 		}
 	}
-	}
-	fflush(stdout); 
-}
 }
